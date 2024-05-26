93c9a4c0050c41222a7959a94ab6339566e1913b
reedrohlfing <rohlfing98@gmail.com>
2024-05-26 12:01:21 -0500

    Updated About description

--- reed-react-app/src/About.js
+++ reed-react-app/src/About.js
@@ -4,11 +4,13 @@ const About = () => {
       <h1 className="lightText">Welcome!</h1>
       <h1>
         I'm Reed, a Systems Engineer that enjoys discovering how technology
-        works... then improving it.
+        works. I have a background in electrical engineering and SATCOM, but
+        recently started to explore software development and UI design.
       </h1>
       <h1 className="lightText">
         Use the navbar above to get a bit more familiar with my experience, or
-        reach out to me directly ↓
+        reach out to me directly! I'd love to chat design, tennis, music, or
+        potential projects ↓
       </h1>
     </div>
   );
